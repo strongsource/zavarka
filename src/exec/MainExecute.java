0cd80f309660271722220ee7c7fd8335a3b91d42
strongsource <vlad3122009@mail.ru>
2016-01-03 10:22:56 +0200

    Create project
    Создание проекта

---
+++ src/exec/MainExecute.java
@@ -0,0 +1,10 @@
+package exec;
+
+/**
+ * Created by Али Баба on 03.01.2016.
+ */
+public class MainExecute {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/logic/Scanning.java
@@ -0,0 +1,7 @@
+package logic;
+
+/**
+ * Created by Али Баба on 03.01.2016.
+ */
+public class Scanning {
+}
